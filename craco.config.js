ff4c3668dc043108c5c39a3df11d61217f490dbb
刘富炜 <fuwei.liu@happy-seed.com>
2021-03-10 11:20:28 +0800

    chore: 增加alias配置

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,24 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const CracoAlias = require('craco-alias')
+const path = require('path')
+
 module.exports = {
-  style: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
+    plugins: [
+        {
+            plugin: CracoAlias,
+            options: {
+                source: 'tsconfig',
+                baseUrl: './',
+                tsConfigPath: './tsconfig.paths.json',
+            },
+            aliases: {
+                '@': path.resolve(__dirname, './src'),
+            },
+        },
+    ],
+    style: {
+        postcss: {
+            plugins: [require('tailwindcss'), require('autoprefixer')],
+        },
     },
-  },
 }
---
+++ src/utils/test.ts
@@ -0,0 +1,4 @@
+export default function test() {
+    console.log(1)
+    return 1
+}
